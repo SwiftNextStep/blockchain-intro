46efb8d33a29d0c637cd6a37e90c08b924d1095c
Icaro Lavrador <icaro@Icaros-Mac-mini.local>
2021-04-11 15:33:36 +1200

    Sharing the data from the wallet with the Sign and Verify components

--- src/components/WalletSignAndVerify.js
+++ src/components/WalletSignAndVerify.js
@@ -1,15 +1,23 @@
 import { Container } from '@chakra-ui/layout';
 import React from 'react';
+import { useImmer } from 'use-immer';
 import Wallet from './Wallet';
 import WalletSign from './WalletSign';
 import WalletVerify from './WalletVerify';
 
 function WalletSignAndVerify() {
+  const [walletData, setWalletData] = useImmer({
+    publicKey: '',
+    privateKey: '',
+    data: '',
+    signature: '',
+    isSignatureValid: false,
+  });
   return (
     <>
-      <Wallet />
-      <WalletSign />
-      <WalletVerify />
+      <Wallet walletData={walletData} setWalletData={setWalletData} />
+      <WalletSign walletData={walletData} setWalletData={setWalletData} />
+      <WalletVerify walletData={walletData} setWalletData={setWalletData} />
     </>
   );
 }
