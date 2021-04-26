0b02f6fa8cea52fc3ccf2a7fda5d52548463c46f
Icaro Lavrador <icaro@Icaros-Mac-mini.local>
2021-04-26 16:51:06 +1200

    Creating a modal to input our Private Key

---
+++ src/components/BlockchainWithWallet/PrivateKeyModal.js
@@ -0,0 +1,37 @@
+import { Button } from '@chakra-ui/button';
+import { FormControl } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
+import { ModalContent } from '@chakra-ui/modal';
+import { ModalCloseButton } from '@chakra-ui/modal';
+import { ModalFooter } from '@chakra-ui/modal';
+import { ModalBody } from '@chakra-ui/modal';
+import { ModalHeader } from '@chakra-ui/modal';
+import { ModalOverlay } from '@chakra-ui/modal';
+import { Modal } from '@chakra-ui/modal';
+import React, { useRef } from 'react';
+
+function PrivateKeyModal({ isOpen, onOpen, onClose }) {
+  const initialRef = useRef();
+  return (
+    <Modal isOpen={isOpen} initialFocusRef={initialRef}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Sign your transaction</ModalHeader>
+        <ModalBody>
+          <FormControl>
+            <Input ref={initialRef} placeholder='Enter your private key' />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='blue' mr='3'>
+            Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+        <ModalCloseButton />
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default PrivateKeyModal;
--- src/components/BlockchainWithWallet/Transaction.js
+++ src/components/BlockchainWithWallet/Transaction.js
@@ -7,44 +7,67 @@ import { StatDownArrow } from '@chakra-ui/stat';
 import { StatUpArrow } from '@chakra-ui/stat';
 import { VStack } from '@chakra-ui/layout';
 import { LockIcon } from '@chakra-ui/icons';
+import { Modal } from '@chakra-ui/modal';
+import PrivateKeyModal from './PrivateKeyModal';
+import { useDisclosure } from '@chakra-ui/hooks';
 
 function Transaction({ updateValue, transaction }) {
   const { amount, to, from, id } = transaction;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <VStack spacing='0' mb='2'>
-      <HStack spacing='1'>
-        <InputGroup>
+    <>
+      <PrivateKeyModal isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
+      <VStack spacing='0' mb='2'>
+        <HStack spacing='1'>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+              color='gray.300'
+              fontSize='1.2em'
+              children='$'
+            />
+            <Input
+              placeholder='Amount'
+              value={amount}
+              onChange={(e) =>
+                updateValue(id, 'amount', Number(e.target.value))
+              }
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+              color='gray.300'
+              fontSize='1.2em'
+              children={<StatUpArrow color='gray.300' />}
+            />
+            <Input
+              placeholder='from'
+              value={from}
+              onChange={(e) => updateValue(id, 'from', e.target.value)}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+              color='gray.300'
+              fontSize='1.2em'
+              children={<StatDownArrow color='gray.300' />}
+            />
+            <Input
+              placeholder='to'
+              value={to}
+              onChange={(e) => updateValue(id, 'to', e.target.value)}
+            />
+          </InputGroup>
+        </HStack>
+        <InputGroup onClick={onOpen}>
           <InputLeftElement
             pointerEvents='none'
             color='gray.300'
             fontSize='1.2em'
-            children='$'
-          />
-          <Input
-            placeholder='Amount'
-            value={amount}
-            onChange={(e) => updateValue(id, 'amount', Number(e.target.value))}
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents='none'
-            color='gray.300'
-            fontSize='1.2em'
-            children={<StatUpArrow color='gray.300' />}
-          />
-          <Input
-            placeholder='from'
-            value={from}
-            onChange={(e) => updateValue(id, 'from', e.target.value)}
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputLeftElement
-            pointerEvents='none'
-            color='gray.300'
-            fontSize='1.2em'
-            children={<StatDownArrow color='gray.300' />}
+            children={<LockIcon color='gray.300' />}
           />
           <Input
             placeholder='to'
@@ -52,21 +75,8 @@ function Transaction({ updateValue, transaction }) {
             onChange={(e) => updateValue(id, 'to', e.target.value)}
           />
         </InputGroup>
-      </HStack>
-      <InputGroup>
-        <InputLeftElement
-          pointerEvents='none'
-          color='gray.300'
-          fontSize='1.2em'
-          children={<LockIcon color='gray.300' />}
-        />
-        <Input
-          placeholder='to'
-          value={to}
-          onChange={(e) => updateValue(id, 'to', e.target.value)}
-        />
-      </InputGroup>
-    </VStack>
+      </VStack>
+    </>
   );
 }
 
